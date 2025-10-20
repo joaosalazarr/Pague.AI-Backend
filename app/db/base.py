1d5e12153389562efdc9e455292a33ac299ee4de
Voidzinn <voidboyyy@gmail.com>
2025-10-20 12:37:17 -0300

    :hammer: Importa os modelos das tabelas na base do banco de dados

--- app/db/base.py
+++ app/db/base.py
@@ -1,3 +1,10 @@
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
+
+from app.models.charges import Charges
+from app.models.companies import Companies
+from app.models.debtors import Debtors
+from app.models.debts import Debts
+from app.models.transactions import Transactions
+from app.models.users import Users
