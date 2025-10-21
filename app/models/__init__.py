bd87310784830fa1b3740cbe42f861f690bb9845
Voidzinn <voidboyyy@gmail.com>
2025-10-21 19:03:39 -0300

    :hammer: Adiciona arquivo __init__.py para facilitar imports e inicialização segura dos módulos

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .companies import Companies
+from .debts import Debts
+from .users import Users
+from .debtors import Debtors
+from .charges import Charges
+from .transactions import Transactions
