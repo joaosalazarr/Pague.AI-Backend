70fedb70ce7c2a169463a96f06b412a76c36e382
Voidzinn <voidboyyy@gmail.com>
2025-10-23 20:34:58 -0300

    :hammer: Complementa o __init__.py dos schemas com as novas funções criadas

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,3 +1,9 @@
 from .user import UserCreate, UserRead, UserLogin
+from .charge import ChargeCreate, ChargeRead
+from .company import CompanyCreate, CompanyRead
+from .debt import DebtCreate, DebtRead
+from .debtor import DebtorCreate, DebtorRead
+from .transaction import TransactionCreate, TransactionRead
 
-__all__ = ['UserCreate', 'UserRead', 'UserLogin']
+__all__ = ['UserCreate', 'UserRead', 'UserLogin', 'ChargeCreate', 'ChargeRead', 'CompanyCreate', 'CompanyRead',
+           'DebtCreate', 'DebtRead', 'DebtorCreate', 'DebtorRead', 'TransactionCreate', 'TransactionRead']
