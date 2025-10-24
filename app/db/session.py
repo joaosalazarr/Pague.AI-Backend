326df6efdf32bf22f89cbd74ced8c1dc27ca4020
Voidzinn <voidboyyy@gmail.com>
2025-10-24 19:03:33 -0300

    :card_file_box: Cria a função que devolve uma sessão do banco de dados

--- app/db/session.py
+++ app/db/session.py
@@ -5,3 +5,10 @@ from app.core.config import DATABASE_URL
 engine = create_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
