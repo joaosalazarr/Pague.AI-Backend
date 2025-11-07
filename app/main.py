993237a5001a2beac1b817ea7c4aebe4525dfa32
Voidzinn <voidboyyy@gmail.com>
2025-11-07 16:38:01 -0300

    :hammer: Adiciona as novas rotas no main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from app.api.v1 import auth_router, user_router, company_router
+from app.api.v1 import auth_router, user_router, company_router, debt_router, debtor_router
 
 app = FastAPI(title='Pague.AI API')
 
 app.include_router(auth_router.router)
 app.include_router(user_router.router)
 app.include_router(company_router.router)
+app.include_router(debt_router.router)
+app.include_router(debtor_router.router)
