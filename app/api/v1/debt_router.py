cee83ff188061fe9fc5359165bc23574d48afb68
Voidzinn <voidboyyy@gmail.com>
2025-11-07 23:20:27 -0300

    :hammer: Adiciona o endpoint para obter as dívidas passando como parâmetro o id de uma empresa.

--- app/api/v1/debt_router.py
+++ app/api/v1/debt_router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.db.session import get_db
-from app.schemas import DebtCreate, DebtRead
-from app.services.debt_service import create_debt
+from app.schemas import DebtCreate, DebtRead, CompanyDebtRead
+from app.services.debt_service import create_debt, get_company_debts
+from uuid import UUID
 
 
 router = APIRouter(prefix='/debts', tags=['Debts'])
@@ -14,3 +15,11 @@ def register_debt(debt: DebtCreate, db: Session = Depends(get_db)):
     Cadastra uma nova dívida no sistema.
     """
     return create_debt(db=db, debt_input=debt)
+
+
+@router.get('/', response_model=list[CompanyDebtRead], status_code=200)
+def company_debts(company_id: UUID, db: Session = Depends(get_db)):
+    """
+    Obtém as dívidas de uma empresa utilizando seu ID como parâmetro.
+    """
+    return get_company_debts(db=db, company_id=company_id)
--- app/services/debt_service.py
+++ app/services/debt_service.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Session
 from app.models import Debts
-from app.schemas import DebtCreate, DebtRead
-from uuid import uuid4
+from app.schemas import DebtCreate, DebtRead, CompanyDebtRead
+from uuid import uuid4, UUID
+
 
 def create_debt(db: Session, debt_input: DebtCreate) -> DebtRead:
     new_debt = Debts(
@@ -16,3 +17,24 @@ def create_debt(db: Session, debt_input: DebtCreate) -> DebtRead:
     db.refresh(new_debt)
 
     return DebtRead.model_validate(new_debt)
+
+
+def get_company_debts(db: Session, company_id: UUID) -> list[CompanyDebtRead]:
+    debts = db.query(Debts).filter(company_id == Debts.company_id).all()
+
+    result: list[CompanyDebtRead] = []
+    for debt in debts:
+        result.append(
+            CompanyDebtRead(
+                id=debt.id,
+                debt_value=debt.debt_value,
+                debt_status=debt.debt_status,
+                debtor_id=debt.debtor_id,
+                created_at=debt.created_at,
+                company_name=debt.company.company_name,
+                debtor_name=debt.debtor.debtor_name,
+                debtor_cpf=debt.debtor.debtor_cpf
+            )
+        )
+
+    return result
