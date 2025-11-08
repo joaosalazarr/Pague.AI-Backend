0fb46d01b4890fb8e40f15a54ecfa7b72335ea88
Voidzinn <voidboyyy@gmail.com>
2025-11-07 23:21:15 -0300

    :hammer: Adiciona rota de cobranças no main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api.v1 import auth_router, user_router, company_router, debt_router, debtor_router
+from app.api.v1 import auth_router, user_router, company_router, debt_router, debtor_router, charges_router
 
 app = FastAPI(title='Pague.AI API')
 
@@ -8,3 +8,4 @@ app.include_router(user_router.router)
 app.include_router(company_router.router)
 app.include_router(debt_router.router)
 app.include_router(debtor_router.router)
+app.include_router(charges_router.router)
