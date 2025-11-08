e2d511feb2bc016892b8562a9dc32afa8991313c
Voidzinn <voidboyyy@gmail.com>
2025-11-07 23:12:24 -0300

    :hammer: Adiciona novo schema de validação para futura rota de get debts

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,9 +1,10 @@
 from .user import UserCreate, UserRead, UserLogin
 from .charge import ChargeCreate, ChargeRead
 from .company import CompanyCreate, CompanyRead
-from .debt import DebtCreate, DebtRead
+from .debt import DebtCreate, DebtRead, CompanyDebtRead
 from .debtor import DebtorCreate, DebtorRead
 from .transaction import TransactionCreate, TransactionRead
 
 __all__ = ['UserCreate', 'UserRead', 'UserLogin', 'ChargeCreate', 'ChargeRead', 'CompanyCreate', 'CompanyRead',
-           'DebtCreate', 'DebtRead', 'DebtorCreate', 'DebtorRead', 'TransactionCreate', 'TransactionRead']
+           'DebtCreate', 'DebtRead', 'CompanyDebtRead', 'DebtorCreate', 'DebtorRead', 'TransactionCreate',
+           'TransactionRead']
--- app/schemas/debt.py
+++ app/schemas/debt.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
 from .base_read_schema import BaseRead
-from .custom_types import Monetary
+from .custom_types import Monetary, ConstrainedString, CPF
 from uuid import UUID
+from datetime import datetime
 
 
 class Debt(BaseModel):
@@ -15,3 +16,10 @@ class DebtCreate(Debt):
 
 class DebtRead(BaseRead, Debt):
     debt_status: bool
+
+
+class CompanyDebtRead(DebtRead):
+    created_at: datetime
+    company_name: ConstrainedString
+    debtor_name: ConstrainedString
+    debtor_cpf: CPF
