f3754f73d22603e8ae9d2b662717e14b3a62fd16
Bruno Rodrigues <bruno.rsilveira@valid.com>
2025-11-08 11:43:44 -0300

    :bug: fix(cors): add cors rules

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import auth_router, user_router, company_router, debt_router, debtor_router, charges_router
 
 app = FastAPI(title='Pague.AI API')
+
+origins = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router.router)
 app.include_router(user_router.router)
